2cb08379ccd9a3a24289e236963544c3eea7684d
gpoult3 <grace.poulton@umconnect.umt.edu>
2024-10-28 23:47:50 -0600

    Homework 9

---
+++ Homework 9/js/sketch.js
@@ -0,0 +1,67 @@
+function setup()
+{
+    createCanvas(500, 600);
+}
+
+function draw()
+{
+    background(120,45,78);
+    textSize(22)
+    text("Portrait", 10,80);
+
+    fill('rgba(255, 220, 215, 10)');
+    circle(250,100,175);
+
+    fill(255);
+    circle(200,75,25);
+
+    fill(255);
+    circle(285,75,25);
+
+    strokeWeight(5);
+    fill(0);
+    point(205,75);
+    point(290,75);
+
+    fill(255);
+    triangle(245,95,240,85,250,85)
+
+    line(240,150,260,150);
+
+    fill(51);
+    ellipse(250,25,175,75);
+
+    fill(51);
+    triangle(130,175,175,50,190,175);
+
+    fill(51);
+    triangle(325,50,360,175,325,175);
+
+    line(130,175,175,50);
+    line(325,50,360,175);
+
+    fill(10,24,120);
+    rect(200,185,100,150);
+
+
+
+    fill(255);
+    rect(220,200,50,100);
+
+    fill('rgba(255, 220, 215, 10)');
+    rect(300,185,20,100);
+
+    fill('rgba(255, 220, 215, 10)');
+    rect(180,185,20,100);
+
+    fill(51);
+    rect(200,335,20,100);
+
+    fill(51);
+    rect(280,335,20,100);
+
+    fill(255);
+    textSize(23);
+    text("Grace Poulton", 270,500);
+
+}
